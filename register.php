95ff417221b9734867092787d4142cf6370b3e50
pawcio <pawelzarzycki89@gmail.com>
2019-11-28 21:11:30 +0100

    add jquery library to project

--- register.php
+++ register.php
@@ -18,8 +18,10 @@
 <html>
 
 <head>
+
     <title>Welcome to Slotify</title>
     <link rel="stylesheet" type="text/css" href="assets/css/register.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 
 <body>
